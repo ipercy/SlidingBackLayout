72eae48471270b9e340679591cb955c077550c41
iamxh <huang1244@163.com>
2016-08-31 13:53:18 +0800

    add LocationHelp

---
+++ src/com/isky45/SlidingBackLayout/LocationHelper.java
@@ -0,0 +1,27 @@
+package com.isky45.SlidingBackLayout;
+
+/**
+ * User: hqs
+ * Date: 2016/8/31
+ * Time: 13:36
+ */
+public class LocationHelper {
+    private String lon;
+    private String lat;
+
+    public String getLon() {
+        return lon;
+    }
+
+    public void setLon(String lon) {
+        this.lon = lon;
+    }
+
+    public String getLat() {
+        return lat;
+    }
+
+    public void setLat(String lat) {
+        this.lat = lat;
+    }
+}
